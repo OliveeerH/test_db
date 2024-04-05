58edc537ad4ed235357e9f21d6b05affd25d87e0
OliveeerH <2410177@cesba-queretaro.edu.mx>
2024-04-05 10:09:33 -0600

    1 commit

---
+++ src/main/java/com/mycompany/test_db/Test_db.java
@@ -0,0 +1,32 @@
+package com.mycompany.test_db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Test_db{
+     public static void main(String[] args) throws ClassNotFoundException, SQLException
+ { 
+    try {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/STUDENT";
+        String user = "root";
+        String pass = "cesba";
+        System.out.println("Java Tratando de Conectar!!!");
+        
+        Class.forName(driver);
+        
+        Connection connection = DriverManager.getConnection(url, user, pass);
+        
+        System.out.println("Java Conectando...");
+    }catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+ }   
+} 
+
+    
+
+   
